774aac9ec347ee47708a9c3168985190f564ec74
Ольга Гегенава <zaharova.mail@mail.ru>
2022-10-31 17:31:29 +0700

    тест

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCountIfAmountBelow1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountIfAmountBelow1000_2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountIfAmountEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountIfAmountAbove1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
